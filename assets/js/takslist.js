4e49f40b14779c9742ae7392e3e8964c8f94c65b
Liviadbelao <125027552+Liviadbelao@users.noreply.github.com>
2023-08-14 10:27:16 -0300

    feat: js first step done

--- assets/js/takslist.js
+++ assets/js/takslist.js
@@ -1,11 +1,13 @@
 class TaskList{
     constructor(doing){
-        this.doing = document.getElementById("InputAdd").value;
+        this.doing = doing;
     }
-
+ 
 }
 
 function addTaskList(){
-    const list = new TaskList();
-    console.log(list)
+const task = document.getElementById("InputAdd").value;
+    const list = new TaskList(task);
+  console.log(list)
+  document.getElementById('result').innerHTML += `<div id="results"><p id="list1">${list.doing}</p><buttorn id="green"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg></button></div>`
 }
