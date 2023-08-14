d2dbc187d7be40d3414cef8ad733dcdd0fa1048a
Liviadbelao <125027552+Liviadbelao@users.noreply.github.com>
2023-08-14 14:25:43 -0300

    feat: trying

--- assets/js/takslist.js
+++ assets/js/takslist.js
@@ -1,13 +1,121 @@
-class TaskList{
-    constructor(doing){
+/* class TaskList {
+    constructor(doing, status, id) {
         this.doing = doing;
+        this.status = false;
+        this.id = id;
     }
- 
+  
+
+} */
+
+
+
+/* class Lists {
+    constructor() {
+        this.arrList = [];
+
+    }
+
+    addArr(things) {
+        this.arrList.push(things)
+    }
+
+    removeTask(index) {
+        posts.splice(index, 1);
+        addTaskList();
+    }
+
+    checkedTask(status){
+        status = false;
+        if (status == true) {
+            document.getElementById("green").style.backgroundColor = "green";
+            status = false;
+        } else {
+            document.getElementById('green').style.backgroundColor = "#2a2a2a";
+            status = true;
+        }
+    }
+
+    noexib(){
+        if(this.arrList == ''){
+            document.getElementById("p1").innerHTML = "Insira um título para sua tarefa!"
+        }
+    }
+} */
+
+function addTaskList() {
+    const task = document.getElementById("InputAdd").value;
+    const list = new TaskList(task);
+   
+
+    console.log(list)
+    document.getElementById('result').innerHTML += `<div id="results"><p id="list1">${list.doing}</p><button id="green" onclick="exib()"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#f5f5f5}</style><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg></button> <button onclick="remove()" id="clean"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#f7f9fd}</style><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg><buton</div>`;
+    /*   list.getEmpty(); */
+    document.getElementById("InputAdd").value = '';
+    const list1 = new Lists(task);
+    list1.noexib()
+    list1.checkedTask()
 }
 
-function addTaskList(){
-const task = document.getElementById("InputAdd").value;
-    const list = new TaskList(task);
-  console.log(list)
-  document.getElementById('result').innerHTML += `<div id="results"><p id="list1">${list.doing}</p><buttorn id="green"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg></button></div>`
+
+
+
+function exib(){
+    const list1 = new Lists(task);
+
+    list1.checkedTask()
+}
+
+
+
+function remove() {
+    const task = document.getElementById("InputAdd").value;
+    const arrLists = new Lists(task);
+    arrLists.getArr(task);
+    console.log(arrLists)
+    document.getElementById("results").style.display = "none";
+    arrLists.removeTask();
+}
+
+class TaskList {
+    constructor(doing, status, id) {
+        this.doing = doing;
+        this.status = false;
+        this.id = id;
+    }
+  
+
+}
+//class metodos e array
+class Lists {
+    constructor() {
+        this.arrList = [];
+
+    }
+
+    addArr(things) {
+        this.arrList.push(things)
+    }
+
+    removeTask(index) {
+        posts.splice(index, 1);
+        addTaskList();
+    }
+
+    checkedTask(status){
+        status = false;
+        if (status == true) {
+            document.getElementById("green").style.backgroundColor = "green";
+            status = false;
+        } else {
+            document.getElementById('green').style.backgroundColor = "#2a2a2a";
+            status = true;
+        }
+    }
+
+    noexib(){
+        if(this.arrList == ''){
+            document.getElementById("p1").innerHTML = "Insira um título para sua tarefa!"
+        }
+    }
 }
